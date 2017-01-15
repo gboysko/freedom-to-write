7ab43c578b54aac0813b7f3c1c371d69618ce88f
Glenn Boysko <gboysko@gmail.com>
2017-01-15 18:28:10 -0500

    Restructured to push Freedom integration into its own module

---
+++ freedom.js
@@ -0,0 +1,208 @@
+const phantom = require('phantom');
+const debug = require('debug')('freedom-to-write:freedom');
+const phantomDebug = require('debug')('freedom-to-write:phantom');
+const log = phantomDebug;
+const Promise = require('bluebird');
+const rp = require('request-promise');
+
+// Phantom Creation options
+const phantomArgs = ['--ignore-ssl-errors=yes', '--load-images=no'],
+	phantomJsConfig = { logLevel: 'warn', logger: { warn: log, debug: log, info: log, error: log}};
+
+const FREEDOM_URL_SIGNIN = 'https://freedom.to/sign-in';
+const FREEDOM_URL_SESSION = 'https://freedom.to/session';
+// const FREEDOM_URL_FREEDOM = 'https://freedom.to/freedom';
+const FREEDOM_URL_DEVICES = 'https://freedom.to/devices';
+const FREEDOM_URL_FILTER_LISTS = 'https://freedom.to/filter_lists';
+
+module.exports = class FreedomIntegration {
+	// Initialize Phantom...
+	initialize() {
+		const methodName = 'initialize';
+
+		// Create a PhantomJS instance...
+		return phantom.create(phantomArgs, phantomJsConfig).then(ph => {
+			// Status...
+			debug('%s: %s', methodName, 'Succesfully created PhantomJS');
+
+			// Hold a reference to the phantom instance
+			this.phInstance = ph;
+
+			// Try to create a new page instance
+			return this.phInstance.createPage().then(page => {
+				// Status...
+				debug('%s: %s', methodName, 'Successfully created PhantomJS page');
+
+				// Hold a reference to the page instance
+				this.pageInstance = page;
+			});
+		}).catch(err => {
+			// Log it...
+			debug('%s: %s', methodName, `Unable to initialize Freedom: ${err}`);
+
+			throw new Error('Unable to initialize Freedom. Enable DEBUG for details.');
+		});
+	}
+
+	// Submit the Freedom Login form (filling in email and password)
+	submitLoginForm(email, password) {
+		return this.pageInstance.evaluate(function(email, password) {
+			/* eslint-disable no-undef */
+			var emailInput = document.querySelector('input[type=email]');
+			var passwordInput = document.querySelector('input[type=password]');
+			var loginForm = document.getElementById('login-form');
+
+			// Put our data into the page...
+			emailInput.value = email;
+			passwordInput.value = password;
+			loginForm.submit();
+		}, email, password);
+	}
+
+	// Handle a received resource
+	onResourceReceived(resolve, reject, response) {
+		const methodName = 'onResourceReceived';
+
+		// Debugging...
+		// debug('%s: %s', methodName, `Invoking onResourceReceived with this.constructor.name=${this.constructor.name}`);
+
+		// Check to see if the resource URL is our session API Ajax call
+		if (response.url === FREEDOM_URL_SESSION && response.stage === 'end') {
+			// Check for a valid session
+			const sessionStatusCode = response.status;
+
+			// Record our session state on our Freedom instance...
+			this.validSession = sessionStatusCode < 400;
+
+			// If invalid, get out now...
+			if (!this.validSession) {
+				// Log it...
+				debug('%s: %s', methodName, `Invalid status code from session page: ${sessionStatusCode}`);
+
+				// Reject the promise...
+				reject(new Error ('Login failed'));
+				return;
+			}
+
+			// Log it...
+			debug('%s: %s', methodName, 'Successfully logged into Freedom.');
+
+			// Pull out the Set-Cookie key
+			const setCookie = response.headers.filter(o => o.name === 'Set-Cookie').map(o => o.value)[0];
+
+			// Put it into our cookie map
+			this.cookieMap = setCookie.split('\n').map(e => e.split(';')[0]).reduce((map, elem) => {
+				// Split by equal sign...
+				const a = elem.split('=');
+
+				// Put the name=value in the map
+				map[a[0]] = a[1];
+
+				return map;
+			}, {});
+
+			// Compute the value of the Cookies header
+			this.cookies = Object.keys(this.cookieMap).map(k => `${k}=${this.cookieMap[k]}`).join('; ');
+
+			// Log it...
+			debug('%s: %s', methodName, `cookieMap:\n${Object.keys(this.cookieMap).map(k => `${k}=${this.cookieMap[k]}`).join('\n')}`);
+			debug('%s: %s', methodName, `cookies: ${this.cookies}`);
+
+			resolve();
+		}
+	}
+
+	// Get our request options
+	getRequestOptions(uri) {
+		return {
+			uri,
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+				'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+				'Cookie': this.cookies
+			},
+			json: true
+		};
+	}
+
+	// Get the Devices JSON
+	getDevices() {
+		const methodName = 'getDevices';
+
+		return rp(this.getRequestOptions(FREEDOM_URL_DEVICES)).then(deviceJson => {
+			// Log it...
+			debug('%s: %s', methodName, `Device JSON: ${JSON.stringify(deviceJson)}`);
+
+			// Construct our device map
+			this.deviceMap = (deviceJson.devices || []).reduce((map, elem) => {
+				map[elem.name] = elem.id;
+
+				return map;
+			}, {});
+
+			// Log it...
+			debug('%s: %s', methodName, `Device Map: ${JSON.stringify(this.deviceMap)}`);
+		}).catch(err => {
+			// Log it...
+			debug('%s: %s', methodName, `Request failed: ${err}`);
+
+			throw new Error('Freedom API call failed');
+		});
+	}
+
+	// Get the Filter Lists JSON
+	getFilterLists() {
+		const methodName = 'getFilterLists';
+
+		return rp(this.getRequestOptions(FREEDOM_URL_FILTER_LISTS)).then(filterListJson => {
+			// Log it...
+			debug('%s: %s', methodName, `Filter Lists JSON: ${JSON.stringify(filterListJson)}`);
+
+			// Construct our filters lists map
+			this.filterListsMap = (filterListJson.filter_lists || []).reduce((map, elem) => {
+				map[elem.name] = elem.id;
+
+				return map;
+			}, {});
+
+			// Log it...
+			debug('%s: %s', methodName, `Filter Lists Map: ${JSON.stringify(this.filterListsMap)}`);
+		}).catch(err => {
+			debug('%s: %s', methodName, `Request failed: ${err}`);
+
+			throw new Error('Freedom API call failed');
+		});
+	}
+
+	// Login to Freedom
+	login(email, password) {
+		const methodName = 'login';
+
+		return Promise.try(() => {
+			// Try to open the sign-in page!
+			return this.pageInstance.open(FREEDOM_URL_SIGNIN);
+		}).then(status => {
+			// Inspect status...
+			if (status !== 'success') {
+				// Log it...
+				debug('%s: %s', methodName, `Unable to open the Freedom Sign-in Page: status=${status}`);
+
+				throw new Error('Login failed. (Internal Error)');
+			}
+
+			// Status...
+			debug('%s: %s', methodName, 'Successfully opened the Freedom Sign-in Page');
+
+			// Try to submit the page!
+			return this.submitLoginForm(email, password);
+		}).then(() => {
+			return new Promise((resolve, reject) => {
+				// Register a function that receives each resource...
+				this.pageInstance.on('onResourceReceived', this.onResourceReceived.bind(this, resolve, reject));
+			});
+		}).then(() => this.getDevices()).then(() => this.getFilterLists()).finally(() => {
+			this.pageInstance.off('onResourceReceived');
+		});
+	}
+};
--- main.js
+++ main.js
@@ -6,8 +6,14 @@ const BrowserWindow = electron.BrowserWindow;
 // Module to receive messages
 const {ipcMain} = electron;
 
-const path = require('path')
-const url = require('url')
+const debug = require('debug')('freedom-to-write:main');
+
+// Module to manage interactions with FreedomIntegration
+const Freedom = require('./freedom');
+
+const path = require('path');
+const url = require('url');
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let dialog, mainWindow;
@@ -18,21 +24,37 @@ let desiredWordCount;
 // Whether we've reached our word count
 let reachedWordCount = false;
 
+// A reference to our Freedom object
+let freedom;
+
 // Handle messages (synch and asynch)
 ipcMain.on('set-word-count', (event, arg) => {
 	desiredWordCount = arg;
 });
-ipcMain.on('get-word-count', (event, arg) => {
+ipcMain.on('get-word-count', (event /*, arg*/) => {
 	event.returnValue = desiredWordCount;
 });
-ipcMain.on('reached-word-count', (event, arg) => {
+ipcMain.on('reached-word-count', (/* event, arg*/) => {
 	reachedWordCount = true;
 	mainWindow.setFullScreen(false);
+});
+ipcMain.on('set-freedom-creds', (event, email, password) => {
+	// Log it...
+	debug(`on set-freedom-creds: email=${email}, password=${password}`);
+
+	// Try to login to Freedom
+	freedom.login(email, password).then(() => {
+		debug('Sending login-success message');
+		dialog.webContents.send('login-success');
+	}).catch(err => {
+		debug('Sending login-failure message');
+		dialog.webContents.send('login-failure', err.message);
+	});
 });
 
 function createWindow () {
 	// Create the browser dialog
-	dialog = new BrowserWindow({width: 400, height: 300, center: true, minimizable: false, maximizable: false});
+	dialog = new BrowserWindow({width: 800, height: 600, center: true, minimizable: false, maximizable: false});
 
 	// and load the index.html of the app.
 	dialog.loadURL(url.format({
@@ -42,7 +64,7 @@ function createWindow () {
 	}));
 
 	// Open the DevTools.
-	// dialog.webContents.openDevTools()
+	// dialog.webContents.openDevTools();
 
 	// Emitted when the window is closed.
 	dialog.on('closed', function () {
@@ -74,27 +96,51 @@ function createWindow () {
 			// when you should delete the corresponding element.
 			dialog = null;
 		}
-	})
+	});
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+	debug('event: ready');
+
+	// Create a new instance of Freedom
+	freedom = new Freedom();
+
+	// Initialize it...
+	/* eslint-disable no-console */
+	freedom.initialize().then(() => {
+		// Status...
+		debug('Freedom initialized.');
+
+		// Create our windows...
+		createWindow();
+	}).catch(err => {
+		console.log(`Freedom failed to initialize: ${err}`);
+
+		throw new Error(err);
+	});
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
+	debug(`event: window-all-closed: desiredWordCount=${desiredWordCount}, reachedWordCount=${reachedWordCount}`);
+
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin' && (!desiredWordCount || reachedWordCount)) {
+	if (desiredWordCount === undefined || reachedWordCount) {
+		debug('calling app.quit()');
 		app.quit();
 	}
 });
 
 app.on('activate', function () {
+	debug('event: activate');
+
 	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (mainWindow === null) {
-		createWindow()
+		createWindow();
 	}
 });
